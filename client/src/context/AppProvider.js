8f085a2c7195b3cc41761436315f2744bf667d3b
MarcoMecenasFilho <marcomecenasfilho@gmail.com>
2022-03-31 16:59:26 -0300

    fix docker env

--- client/src/context/AppProvider.js
+++ client/src/context/AppProvider.js
@@ -3,26 +3,7 @@ import propTypes from 'prop-types';
 import AppContext from './AppContext';
 
 export default function AppProvider({ children }) {
-  const [capturedPokemon, setCapturedPokemon] = useState([{
-    id: 1,
-    name: 'Bulbasaur',
-    image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png',
-  },
-  {
-    id: 5,
-    name: 'Charmeleon',
-    image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/005.png',
-  },
-  {
-    id: 9,
-    name: 'Blastoise',
-    image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/009.png',
-  },
-  {
-    id: 25,
-    name: 'Pikachu',
-    image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png',
-  }]);
+  const [capturedPokemon, setCapturedPokemon] = useState([]);
   const [createOn, setCreateOn] = useState(false);
   const [deleteOn, setDeleteOn] = useState(false);
   
--- server/sequelize/config.js
+++ server/sequelize/config.js
@@ -1,38 +1,24 @@
 require('dotenv').config();
 module.exports = {
   development: {
-    username: 'root',
-    password: '39261171',
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
     database: 'pokemon_safari',
-    host: 'localhost',
+    host: process.env.HOSTNAME,
     dialect: 'mysql',
   },
-  // development: {
-  //   username: process.env.MYSQL_USER,
-  //   password: process.env.MYSQL_PASSWORD,
-  //   database: 'pokemon_safari',
-  //   host: process.env.HOSTNAME,
-  //   dialect: 'mysql',
-  // },
-  // test: {
-  //   database: 'pokemon_safari-test',
-  //   username: process.env.MYSQL_USER,
-  //   password: process.env.MYSQL_PASSWORD,
-  //   host: process.env.HOSTNAME,
-  //   dialect: 'mysql',
-  // },
-    test: {
+  test: {
+    database: 'pokemon_safari-test',
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    host: process.env.HOSTNAME,
+    dialect: 'mysql',
+  },
+  production: {
     username: 'root',
-    password: '39261171',
-    database: 'pokemon_safari_test',
-    host: 'localhost',
+    password: null,
+    database: 'database_production',
+    host: '127.0.0.1',
     dialect: 'mysql',
   },
-  // production: {
-  //   username: 'root',
-  //   password: null,
-  //   database: 'database_production',
-  //   host: '127.0.0.1',
-  //   dialect: 'mysql',
-  // },
 };
